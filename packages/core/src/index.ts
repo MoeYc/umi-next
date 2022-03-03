3f89302d7ccefae58e59576571c779e756ed1005
fz6m <59400654+fz6m@users.noreply.github.com>
2022-03-04 03:22:19 +0800

    chore: force check ci cache

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,3 +4,4 @@ export { GeneratorType, IGeneratorOpts } from './service/generator';
 export * from './service/pluginAPI';
 export * from './service/service';
 export { Env, IAdd, IEvent, IModify, IRoute } from './types';
+export const a = 1;
